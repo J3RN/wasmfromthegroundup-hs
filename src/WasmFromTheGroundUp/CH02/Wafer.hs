ec0239828a1356af5ee42022f800d8aafa1a90b9
Jonathan Arnett <j3rn@j3rn.com>
2025-04-23 00:25:49 -0400

    Use Parsec.Token in CH02

--- src/WasmFromTheGroundUp/CH02/Wafer.hs
+++ src/WasmFromTheGroundUp/CH02/Wafer.hs
@@ -2,9 +2,11 @@ module WasmFromTheGroundUp.CH02.Wafer (compile, Err (..)) where
 
 import           Data.ByteString.Builder      (Builder)
 import           Data.Int                     (Int32)
-import           Data.Text                    (Text)
-import           Text.Parsec                  (digit, many1, runParser)
-import           Text.Parsec.Text             (Parser)
+import           Text.Parsec                  (runParser)
+import           Text.Parsec.Language         (emptyDef)
+import           Text.Parsec.String           (Parser)
+import           Text.Parsec.Token            (LanguageDef, TokenParser,
+                                               integer, makeTokenParser)
 import           WasmFromTheGroundUp.CH01.Nop hiding (main)
 
 newtype Err = Error {_message :: String} deriving (Show, Eq)
@@ -13,9 +15,9 @@ main :: Parser Int32
 main = number
 
 number :: Parser Int32
-number = read <$> many1 digit
+number = fromIntegral <$> integer wafer
 
-compile :: Text -> Either Err Builder
+compile :: String -> Either Err Builder
 compile source = case parseResult of
   Right val -> Right (encode (modu val))
   Left e    -> Left (Error (show e))
@@ -28,3 +30,13 @@ compile source = case parseResult of
         , _exportSection = ExportSection [FunctionExport "main" 0]
         , _codeSection = CodeSection [CodeEntry [] [I32Const val, End]]
         }
+
+-- We convert the waferDef (declarative description of the syntax) to a token
+-- parser (includes helpful utilities and consumes whitespace between elements).
+wafer :: TokenParser ()
+wafer = makeTokenParser waferDef
+
+-- We would customize this to define comment syntax, operators, etc, but we
+-- don't need any of that (yet)
+waferDef :: LanguageDef ()
+waferDef = emptyDef
